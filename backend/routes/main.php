f2c5a667932e6ec61e3c0232689bf3d7b89be320
mikael <mikael.quadros@gmail.com>
2023-05-31 02:05:02 -0300

    Allowed CORS on backend

--- backend/routes/main.php
+++ backend/routes/main.php
@@ -2,6 +2,10 @@
 
 use flight\Flight;
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
 require_once 'api/enterprise.php';
 require_once 'api/state.php';
 require_once 'api/city.php';
