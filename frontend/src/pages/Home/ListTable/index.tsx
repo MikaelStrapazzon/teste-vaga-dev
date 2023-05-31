fbd50a05a8e69c7ecb16e0b5bf085ca10b340a97
mikael <mikael.quadros@gmail.com>
2023-05-31 02:05:25 -0300

    Enterprise List in frontend

--- frontend/src/pages/Home/ListTable/index.tsx
+++ frontend/src/pages/Home/ListTable/index.tsx
@@ -1,8 +1,45 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Container} from "./styles";
+import {api} from "../../../services/api";
+
+interface enterprise {
+  cnpj: string
+  name: string
+}
 
 export default function ListTable() {
+  const [enterprises, setEnterprises] = useState<enterprise[]>([]);
+
+  useEffect(() => {
+    api.get("enterprise")
+      .then( response => setEnterprises(response.data) )
+  }, []);
+
   return (
-    <Container></Container>
+    <Container>
+      <h2>Empresas Cadastradas</h2>
+
+      <table>
+        <thead>
+          <tr>
+            <th>CNPJ</th>
+            <th>Nome da Empresa</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {enterprises.map(enterprise => (
+            <tr>
+              <td>{enterprise.cnpj}</td>
+              <td>{enterprise.name}</td>
+              <td>
+                <button>Editar</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Container>
   );
 }
