070e29ada1524f185cbb4a9c57ff4bdbf3c1fac2
mikael <mikael.quadros@gmail.com>
2023-05-31 02:48:02 -0300

    Cancel button to clear fields

--- frontend/src/pages/Home/FormCreate/index.tsx
+++ frontend/src/pages/Home/FormCreate/index.tsx
@@ -13,6 +13,23 @@ interface city {
 }
 
 export default function FormCreate() {
+  const [cnpj, setCnpj] = useState('');
+  const [name, setName] = useState('');
+  const [cep, setCep] = useState('');
+  const [address, setAddress] = useState('');
+  const [number, setNumber] = useState('');
+  const [neighborhood, setNeighborhood] = useState('');
+  const [city, setCity] = useState('');
+
+  const handleClearForm = () => {
+    setCnpj('');
+    setName('');
+    setCep('');
+    setAddress('');
+    setNumber('');
+    setNeighborhood('');
+  };
+
   const [states, setStates] = useState<state[]>([]);
   const [stateSelected, setStateSelected] = useState<number>();
 
@@ -24,7 +41,6 @@ export default function FormCreate() {
   }, []);
 
   useEffect(() => {
-    console.log(stateSelected)
     if (!stateSelected)
       return
 
@@ -38,32 +54,32 @@ export default function FormCreate() {
 
         <div>
           <label htmlFor='form_cnpj'>CNPJ</label>
-          <input id='form_cnpj' type='text' name='cnpj'/>
+          <input id='form_cnpj' type='text' value={cnpj} onChange={(e) => setCnpj(e.target.value)}/>
         </div>
 
         <div>
           <label htmlFor='form_name'>Nome da Empresa</label>
-          <input id='form_name' type='text' name='name'/>
+          <input id='form_name' type='text' value={name} onChange={(e) => setName(e.target.value)}/>
         </div>
 
         <div>
           <label htmlFor='form_cep'>CEP</label>
-          <input id='form_cep' type='text' name='cep'/>
+          <input id='form_cep' type='text' value={cep} onChange={(e) => setCep(e.target.value)}/>
         </div>
 
         <div>
           <label htmlFor='form_address'>Endereço</label>
-          <input id='form_address' type='text' name='address'/>
+          <input id='form_address' type='text' value={address} onChange={(e) => setAddress(e.target.value)}/>
         </div>
 
         <div>
           <label htmlFor='form_number'>Número</label>
-          <input id='form_number' type='text' name='number'/>
+          <input id='form_number' type='text' value={number} onChange={(e) => setNumber(e.target.value)}/>
         </div>
 
         <div>
           <label htmlFor='form_neighborhood'>Bairro</label>
-          <input id='form_neighborhood' type='text' name='neighborhood'/>
+          <input id='form_neighborhood' type='text' value={neighborhood} onChange={(e) => setNeighborhood(e.target.value)}/>
         </div>
 
         <div>
@@ -78,13 +94,16 @@ export default function FormCreate() {
 
         <div>
           <label htmlFor='form_city'>Cidade</label>
-          <select id='form_city' name='city_id'>
+          <select id='form_city' onChange={(e) => setCity(e.target.value)}>
             {cities.map(city => (
               <option value={city.id}>{city.name}</option>
             ))}
-            <option hidden selected>Selecione um estado</option>
+            <option hidden selected value='0'>Selecione um estado</option>
           </select>
         </div>
+
+        <input type='button' value='Cancelar' onClick={handleClearForm}/>
+        <input type='submit' value='Salvar'/>
 
       </form>
     </Container>
