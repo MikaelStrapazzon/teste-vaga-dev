f35524bd02067edf347b14230bc9914d0f0bb3e2
mikael <mikael.quadros@gmail.com>
2023-06-05 20:53:34 -0300

    import Global styles

--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -1,53 +1,52 @@
 import {createGlobalStyle} from "styled-components"
 
 export const GlobalStyle = createGlobalStyle`
-    :root {
-        --background: #F0F2F5;
-        --cinza: #ddd;
-    }
+  :root {
+    --background: #f7f9fa;
+  }
 
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-    html {
-        @media (max-width: 1080px) {
-            font-size: 93.75%;
-        }
-        @media (max-width: 720px) {
-            font-size: 87.5%;
-        }
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
     }
-
-    body {
-        background: var(--background);
-        -webkit-font-smoothing: antialiased;
+    @media (max-width: 720px) {
+      font-size: 87.5%;
     }
+  }
 
-    body, input, textarea, button {
-        font-family: "Poppins", sans-serif;
-        font-weight: 400;
-    }
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
 
-    h1, h2, h3, h4, h5, h6, strong {
-        font-weight: 600;
-    }
+  body, input, textarea, button {
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+  }
 
-    button {
-        cursor: pointer;
-    }
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
 
-    [disabled] {
-        opacity: 0.6;
-        cursor: not-allowed;
-    }
+  button {
+    cursor: pointer;
+  }
 
-    #root {
-        height: 100vh;
-        width: 100vw;
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
 
-        display: flex;
-    }
+  #root {
+    height: 100vh;
+    width: 100vw;
+
+    display: flex;
+  }
 `
